28db1e27b1bd731b3e1df1abd57e8d441cba6c55
marysgonzalez <mary.sgonzalez2@gmail.com>
2014-12-09 18:54:58 -0430

    Gets

--- app/src/main/java/com/ucab/ecotoolbox/ecotoolbox/MapsActivity.java
+++ app/src/main/java/com/ucab/ecotoolbox/ecotoolbox/MapsActivity.java
@@ -2,12 +2,18 @@ package com.ucab.ecotoolbox.ecotoolbox;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -18,6 +24,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -45,21 +52,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.Provider;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+    private String urlImg;
+    private String descrip = "Not found";
+    private Bitmap myBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-        // Showing status
         if(status!= ConnectionResult.SUCCESS){ // Google Play Services are not available
 
             int requestCode = 10;
@@ -70,10 +80,10 @@ public class MapsActivity extends FragmentActivity {
         else{ setUpMapIfNeeded();}
     }
 
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-    }
+//    public void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -122,7 +132,9 @@ public class MapsActivity extends FragmentActivity {
                 // Get Current Location
 //            myLocation = locationManager.getLast
                 myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
+                if(myLocation==null){
+                    myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
 //            }else{
 //                Toast.makeText(getApplicationContext(), "Revise GPS y su conexion a internet",
 //                        Toast.LENGTH_SHORT).show();
@@ -130,25 +142,24 @@ public class MapsActivity extends FragmentActivity {
 
         }catch(Exception e){
             Toast.makeText(getApplicationContext(), "Revise GPS y su conexion a internet",
-                    Toast.LENGTH_SHORT).show();
+                    Toast.LENGTH_LONG).show();
             
         }
         return myLocation;
 
     }
 
-    public static class PlaceholderFragmentMaps extends Fragment {
 
-        public PlaceholderFragmentMaps() {
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
         }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-            return rootView;
-        }
+        return false;
     }
+
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
@@ -167,25 +178,27 @@ public class MapsActivity extends FragmentActivity {
 
         if(myLocation==null){
             Toast.makeText(getApplicationContext(), "Ha ocurrido un error. Revise GPS y su conexion a internet",
-                    Toast.LENGTH_SHORT).show();
-
+                    Toast.LENGTH_LONG).show();
+            return;
         }
         // set map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         // Get latitude of the current location
-        double latitude = myLocation.getLatitude();
+        final double latitude = myLocation.getLatitude();
 
         // Get longitude of the current location
-        double longitude = myLocation.getLongitude();
-//        ObtenerPuntos op = new ObtenerPuntos((float)latitude,(float)longitude,5);
-//        op.execute();
+        final double longitude = myLocation.getLongitude();
+
+
         // Create a LatLng object for the current location
         LatLng latLng = new LatLng(latitude, longitude);
 
         // Show the current location in Google Map
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
+        ObtenerPuntos op = new ObtenerPuntos((float)latitude,(float)longitude,5);
+        op.execute();
         // Zoom in the Google Map
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
@@ -194,12 +207,7 @@ public class MapsActivity extends FragmentActivity {
 
                 markerOptions.position(point);
 
-                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.basura));
-
-                markerOptions.title("Basura");
-
                 mMap.addMarker(markerOptions);
-
                 FragmentoSubirFoto subirfoto = new FragmentoSubirFoto();
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 fragmentManager.beginTransaction()
@@ -207,36 +215,93 @@ public class MapsActivity extends FragmentActivity {
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                         .addToBackStack(null)
                         .commit();
+                subirfoto.setLon((float)point.longitude);
+                subirfoto.setLat((float)point.latitude);
+
+
             }
         });
 
         mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-
 
         LatLng coordinate = new LatLng(latitude, longitude);
         CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 12);
         mMap.animateCamera(yourLocation);
     }
 
-    public class ObtenerPuntos  extends AsyncTask<String,Integer,JSONObject> {
+//    class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+//        ImageView bmImage;
+//
+//        public DownloadImageTask(ImageView bmImage) {
+//            this.bmImage = bmImage;
+//        }
+//
+//
+//        protected Bitmap doInBackground(String... urls) {
+//            String urldisplay = urls[0];
+//            Bitmap mIcon11 = null;
+//            try {
+//                InputStream in = new java.net.URL(urldisplay).openStream();
+//                mIcon11 = BitmapFactory.decodeStream(in);
+//            } catch (Exception e) {
+//                Log.e("Error", e.getMessage());
+//                e.printStackTrace();
+//            }
+//            return mIcon11;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Bitmap result) {
+//            super.onPostExecute(result);
+//            bmImage.setImageBitmap(result);
+//        }
+//    }
 
-        private Integer radio;
-        private Float lon;
-        private Float lat;
-        public ObtenerPuntos(float lat , float lon , int radio){
-            this.radio = radio;
-            this.lon = lon;
-            this.lat = lat;
+
+
+    public class ObtenerDetalle  extends AsyncTask<String,Integer,JSONObject> {
+
+        private Integer id;
+
+        public ObtenerDetalle(int id) {
+            this.id = id;
+
         }
 
         @Override
         protected JSONObject doInBackground(String... params) {
-            String url = "http://api2-ecotoolbox.rhcloud.com/api/nearbyPoints/"+this.lat.toString()+"/"+this.lon.toString()+"/"+this.radio.toString();
+            JSONObject respuesta = null;
+//            if (isOnline()) {
+            String url = "http://api2-ecotoolbox.rhcloud.com/api/points/" + this.id.toString();
             BufferedReader in = null;
-            JSONObject respuestaPunto = null;
             InputStream is = null;
-            try
-            {
+
+//            URL imageUrl = null;
+//            HttpURLConnection conn = null;
+//
+//            try {
+//
+//                imageUrl = new URL(urlImg);
+//                conn = (HttpURLConnection) imageUrl.openConnection();
+//                conn.connect();
+//
+//                BitmapFactory.Options options = new BitmapFactory.Options();
+//                options.inSampleSize = 5; // el factor de escala a minimizar la imagen, siempre es potencia de 2
+//
+//                myBitmap = BitmapFactory.decodeStream(conn.getInputStream(), new Rect(0, 0, 0, 0), options);
+//                ImageView imageView = (ImageView) findViewById(R.id.imagen);
+//                Log.d("BIT",myBitmap.toString());
+//                imageView.setImageBitmap(myBitmap);
+////                img.setImageBitmap(imagen);
+//
+//            } catch (IOException e) {
+//
+//                e.printStackTrace();
+//
+//            }
+
+            try {
+
                 HttpClient client = new DefaultHttpClient();
                 HttpGet request = new HttpGet();
                 request.setURI(new URI(url));
@@ -244,17 +309,15 @@ public class MapsActivity extends FragmentActivity {
                 HttpEntity entidad = response.getEntity();
                 is = entidad.getContent();
 
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
 
-            try{// PROCESO LA RESPUESTA DEL SERVER
+            try {// PROCESO LA RESPUESTA DEL SERVER
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
                 StringBuilder sb = new StringBuilder();
                 String line = "0";
 
@@ -263,59 +326,183 @@ public class MapsActivity extends FragmentActivity {
                 }
                 is.close();
 
-                String result=sb.toString();
-                respuestaPunto  = new JSONObject(result); //CREO EL JSONObject para luego obtener el url
+                String result = sb.toString();
+                respuesta = new JSONObject(result); //CREO EL JSONObject para luego obtener el url
 
 
-            }catch(Exception e){
 
+            } catch (Exception e) {
+                Log.d("Problema Servidor: Get", "Servidor offline");
+                Log.d("Prob", e.toString());
             }
+
+//            }
+//            else{
+//                Toast.makeText(getApplicationContext(), "Revise su conexión a internet",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+            return respuesta;
+        }
+
+        protected void onPostExecute(JSONObject respuesta) {
+            super.onPostExecute(respuesta);
+            try {
+                String status = respuesta.getString("status");
+                if (status.compareTo("200")== 0){
+                    JSONObject arregloPtos = respuesta.getJSONObject("data");
+
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        urlImg = arregloPtos.getString("foto");
+                        descrip = new String (arregloPtos.getString("descripcion"));
+                        Log.d("copy", descrip);
+
+                }else if (status.compareTo("404")== 0)
+                {
+                    Toast.makeText(getApplicationContext(), "Ha ocurrido un error obteniendo puntos del servidor",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.d("try obtener detalle",e.toString());
+            }
+        }
+    }
+
+
+
+    public class ObtenerPuntos  extends AsyncTask<String,Integer,JSONObject> {
+
+        private Integer radio;
+        private Float lon;
+        private Float lat;
+
+        public ObtenerPuntos(float lat, float lon, int radio) {
+            this.radio = radio;
+            this.lon = lon;
+            this.lat = lat;
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            JSONObject respuestaPunto = null;
+//            if (isOnline()) {
+                String url = "http://api2-ecotoolbox.rhcloud.com/api/nearbyPoints/" + this.lat.toString() + "/" + this.lon.toString() + "/" + this.radio.toString();
+                BufferedReader in = null;
+                InputStream is = null;
+                try {
+
+                    HttpClient client = new DefaultHttpClient();
+                    HttpGet request = new HttpGet();
+                    request.setURI(new URI(url));
+                    HttpResponse response = client.execute(request);
+                    HttpEntity entidad = response.getEntity();
+                    is = entidad.getContent();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+
+                try {// PROCESO LA RESPUESTA DEL SERVER
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                    StringBuilder sb = new StringBuilder();
+                    String line = "0";
+
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    is.close();
+
+                    String result = sb.toString();
+                    respuestaPunto = new JSONObject(result); //CREO EL JSONObject para luego obtener el url
+
+
+                } catch (Exception e) {
+                    Log.d("Problema Servidor", "Servidor offline");
+                }
+
+//            }
+//            else{
+//                Toast.makeText(getApplicationContext(), "Revise su conexión a internet",
+//                        Toast.LENGTH_SHORT).show();
+//            }
             return respuestaPunto;
         }
+
+
 
 
         protected void onPostExecute(JSONObject respuestaPuntos) {
             super.onPostExecute(respuestaPuntos);
             try {
                 String status = respuestaPuntos.getString("status");
-                if (status=="200"){
+                if (status.compareTo("200")== 0){
                     JSONArray arregloPtos = respuestaPuntos.getJSONArray("data");
-                        for (int i = 0 ; arregloPtos.getJSONObject(i)!=null ; i++){// ojo
+
+                        for (int i = 0 ; i<arregloPtos.length(); i++){// ojo
+
                             MarkerOptions markerOptions = new MarkerOptions();
                             double lat = arregloPtos.getJSONObject(i).getDouble("lat");
                             double lon = arregloPtos.getJSONObject(i).getDouble("lng");
 
                             markerOptions.position(new LatLng(lat, lon));
-
+                            final int idDetalle = arregloPtos.getJSONObject(i).getInt("id");
+                            ObtenerDetalle opi = new ObtenerDetalle(arregloPtos.getJSONObject(i).getInt("id"));
+                            opi.execute();
+                            Log.d("d", descrip);
+//                            Log.d("ID",arregloPtos.getJSONObject(i).getString("id"));
                             switch(arregloPtos.getJSONObject(i).getInt("categoria")){
-                                case 0: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.basura));
-                                        markerOptions.title("Basura");
+                                case 0: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_basura));
+                                        markerOptions.title("Basura").snippet(descrip);
+
                                         break;
-                                case 1: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_evento));
-                                        markerOptions.title("Evento");
+                                case 1: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_evento));
+                                        markerOptions.title("Evento").snippet(descrip);
                                         break;
-                                case 2: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_reciclaje));
-                                        markerOptions.title("Reciclar");
+                                case 2: markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_reciclaje));
+                                        markerOptions.title("Reciclar").snippet(descrip);
                                         break;
                             }
 
-                           // markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.basura));
-
+//                            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+//
+//                                // Use default InfoWindow frame
+//                                @Override
+//                                public View getInfoWindow(Marker arg0) {
+//                                    return null;
+//                                }
+//
+//                                // Defines the contents of the InfoWindow
+//                                @Override
+//                                public View getInfoContents(Marker arg0) {
+//
+//                                    // Getting view from the layout file info_window_layout
+//
+//                                    View v = getLayoutInflater().inflate(R.layout.fragment_map, null);
+////                                    ImageView imageView = (ImageView) findViewById(R.id.imagen);
+////                                    imageView.setImageBitmap(myBitmap);
+////                                    new DownloadImageTask(imageView).execute(urlImg);
+//                                    return v;
+//
+//                                }
+//                            });
 
 
                             mMap.addMarker(markerOptions);
-
-
                     }
 
-                }else if (status=="404")
+                }else if (status.compareTo("404")== 0)
                 {
                     Toast.makeText(getApplicationContext(), "Ha ocurrido un error obteniendo puntos del servidor",
-                            Toast.LENGTH_SHORT).show();
+                            Toast.LENGTH_LONG).show();
                     return;
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
+                Log.d("try obtener puntos",e.toString());
             }
         }
     }
